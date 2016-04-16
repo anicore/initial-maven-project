f346c4f48e67eeb75bf8cef888071563672e9ed2
anicore <Nico.Alpert@gmx.net>
2016-04-16 12:49:58 +0200

    Initial commit

---
+++ src/main/java/de/nalpert/initial/project/App.java
@@ -0,0 +1,13 @@
+package de.nalpert.initial.project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
